51ba0c3087d5fb5877175a4ef32ed55a446891e6
biky-mandal <m.biky50@gmail.com>
2022-02-03 17:28:25 +0530

    Booking page done

--- src/component/input/index.js
+++ src/component/input/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 
-const InputBox = (props) => {
+export const InputBox = (props) => {
     return(
         <div className='user-input-div'>
             <label>{props.title}</label>
@@ -14,4 +14,65 @@ const InputBox = (props) => {
     )
 }
 
-export default InputBox
+export const SelectBoxCountry = (props) => {
+
+    const [country, setCountry] = useState();
+
+    console.log(country);
+    return(
+        <div className='user-input-div'>
+            <label>Visibility to audience ?</label>
+            <select 
+                className='user-select-input'
+                type='select'
+                value={country}
+                onChange={(e) => setCountry(e.target.value)}
+            >
+                {
+                    props.data.map(c => {
+                        return(
+                            <option value={c.name}>{c.unicodeFlag} {c.name}</option>
+                        )
+                    })
+                }
+                <option>Select Visibility</option>
+                <option value='public'>Keep it Public</option>
+                <option value='private'>Keep it Private</option>
+            </select>
+        </div>
+    )
+}
+export const SelectBox = () => {
+
+    const [visibility, setVisibility] = useState();
+
+    return(
+        <div className='user-input-div'>
+            <label>Visibility to audience ?</label>
+            <select 
+                className='user-select-input'
+                type='select'
+                value={visibility}
+                onChange={(e) => setVisibility(e.target.value)}
+            >
+                <option>Select Visibility</option>
+                <option value='public'>Keep it Public</option>
+                <option value='private'>Keep it Private</option>
+            </select>
+        </div>
+    )
+}
+
+
+export const DescriptionBox = (props) => {
+    return(
+        <div className='user-input-div'>
+            <label>{props.title}</label>
+            <textarea 
+                className='user-input-textarea'
+                type='text'
+                rows='4'
+            />
+        </div>
+    )
+}
--- src/container/booking/index.js
+++ src/container/booking/index.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { NavLink } from 'react-router-dom';
 
 import Header from '../../component/header';
-import InputBox from '../../component/input';
+import { DescriptionBox, InputBox, SelectBox, SelectBoxCountry } from '../../component/input';
+import bookingpic from '../../resources/bookingpic.png';
 import './style.css';
 
 const Booking = (props) => {
+
+    const [countryData, setCountryData] = useState();
+
+    useEffect(() => {
+        const getCounties = () => {
+            fetch('https://countriesnow.space/api/v0.1/countries/flag/unicode')
+            .then((res) => res.json())
+            .then(json => {
+                setCountryData(json.data);
+            })
+        } 
+
+        getCounties();
+    },[]);
+
+    console.log(countryData);
+
     return(
         <>
             <Header/>
@@ -14,24 +33,51 @@ const Booking = (props) => {
                     <h3>The short film challange</h3>
                 </div>
                 <div className='booking-main-bottom-div'>
-                        <div className='bmb-left-div'>
-                            <InputBox
-                                title='Film link (Youtube/Vimeo/Google Drive)'
-                            />
+                    <div className='bmb-left-div'>
+                        <InputBox title='Film link (Youtube/Vimeo/Google Drive)'/>
+                        <InputBox title='Title' />
 
-                            <InputBox
-                                title='Title'
-                            />
+                        <img src={bookingpic}/>
+                    </div>
+
+                    <div className='bmb-right-div'>
+                        <SelectBox/>
+                        <DescriptionBox title='Film and Team description' />
+                        <InputBox title='Name'/>
+                        <div className='bmb-right-div-bottom'> 
+                            <div className='bmb-rdb-left'>
+                                <InputBox title='Phone'/>
+                                <InputBox title='Instagram' />
+
+                                {countryData ? 
+                                    <SelectBoxCountry
+                                        data={countryData}
+                                    />
+                                    :
+                                    null
+                                }
+                            </div>
+
+                            <div className='bmb-rdb-right'>
+                                <InputBox title='Email'/>
+                                <InputBox title='Facebook'/>
+                                <InputBox title='Is file password protected ?' />
+                            </div>
+
                         </div>
-
-                        <div className='bmb-right-div'>
-
+                            <label className='terms-agree'>By clicking 'POST NOW' you agree to our 
+                                <NavLink to='terms'> Terms of POST</NavLink>
+                            </label>
+                        
+                        <div className='final-submit-btn-div'>
+                            <button className='free-btn'>POST NOW (FREE)</button>
+                            <button className='paid-btn'>POST NOW (29$)</button>
                         </div>
+                    </div>
                 </div>
             </div>
         </>
-
-    );
+ );
 }
 
 export default Booking;
